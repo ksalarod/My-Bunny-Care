8fd206a135998f01837a8b40dc9dcd2876eba938
Karla Salamanca Rodriguez <156261888+ksalarod@users.noreply.github.com>
2024-03-27 01:04:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 //3.26.24
 
 
-
+//Global  variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
 //Hamburger menu functionfunction menu() {  
  var navlinks = document.getElementById("nav-links");  
   var menuicon = document.getElementById("icon");  
@@ -13,3 +15,17 @@
         } else {     
          navlinks.style.display = "block";     
           menuicon.style.color = "#f6eee4";   
+//Function to display the first answer
+    function   ans1() {  
+     heading.style.display = "block";
+     answer.textContent = "Step back and observe the situation for a few minutes. Wait to see if a parent arrives. If the animal is in immediate danger from a predator or is in the road, remove the baby and bring to a rescue center.";
+    }
+    //Function to display the second answer
+    function ans2() { 
+     heading.style.display = "block";
+     answer.textContent = "You cannot tell if an animal has rabies simply by seeing it. A test must be performed to determine if an animal has rabies. Do not approach wildlife that you suspect might be rabid. Contact us to have the animal removed.";
+    }
+    //Function to display the third answerfunction ans3() {   
+    heading.style.display = "block";
+    
+    answer.textContent = "No. This is a myth. The parents will retrieve the baby bird and place it back in its nest. If the parents do not return, contact us."
